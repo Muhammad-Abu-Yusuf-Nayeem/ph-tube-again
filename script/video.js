576d3ee6af6bbcf172b17e44b8475488dee276ed
assignment-01 <ykhan9463@gmail.com>
2025-07-05 12:44:45 +0600

    add Access Object And Display Dynamic Data To Card

---
+++ script/video.js
@@ -0,0 +1,74 @@
+// 1 - fetch, load and show categories on html
+
+// create loadCategories
+const loadCategories = () => {
+  console.log("load categories");
+
+  //fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories))
+    .catch((error) => console.log(error));
+};
+
+//loadVideos
+const loadVideos = () => {
+  console.log("load videos");
+
+  //fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos))
+    .catch((error) => console.log(error));
+};
+
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("videos");
+  videos.forEach((video) => {
+    console.log(video);
+    const card = document.createElement("div");
+    card.classList = "card card-compact";
+    card.innerHTML = `
+    <figure class= "h-[200px]">
+    <img
+      src=${video.thumbnail}
+      class= "h-full w-full object-cover"
+      alt="Shoes" />
+  </figure>
+  <div class="px-0 py-2 flex gap-2">
+    <div>
+    <img class="w-[40px] h-[40px] rounded-full object-cover" src=${video.authors[0].profile_picture}>
+  </div>
+  <div>
+    <p class="text-lg font-bold">${video.title}</p>
+    <div >
+    <p class="text-gray-600">
+      ${video.authors[0].profile_name}
+    </p>
+    <img src="" alt="">
+  </div>
+  </div>
+  </div>
+    `;
+    videoContainer.append(card);
+  });
+};
+
+//create DisplayCategories
+const displayCategories = (categories) => {
+  const categoryContainer = document.getElementById("categories");
+  //add data in html
+  categories.forEach((item) => {
+    console.log(item);
+    //create button
+    const button = document.createElement("button");
+    button.classList = "btn";
+    button.innerText = item.category;
+
+    // add button to category container
+    categoryContainer.append(button);
+  });
+};
+
+loadCategories();
+loadVideos();
