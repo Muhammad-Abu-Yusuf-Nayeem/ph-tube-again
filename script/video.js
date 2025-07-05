4bd1f36f41bae9fcb3abf41ca13d67a9a43b9c78
assignment-01 <ykhan9463@gmail.com>
2025-07-05 15:00:14 +0600

    Render Conditional Data On Dynamic Card

--- script/video.js
+++ script/video.js
@@ -29,23 +29,33 @@ const displayVideos = (videos) => {
     const card = document.createElement("div");
     card.classList = "card card-compact";
     card.innerHTML = `
-    <figure class= "h-[200px]">
+    <figure class= "h-[200px] relative">
     <img
       src=${video.thumbnail}
       class= "h-full w-full object-cover"
       alt="Shoes" />
+      <span class="absolute right-2 bottom-2 bg-black text-white rounded p-1">${
+        video.others.posted_date
+      }</span>
   </figure>
   <div class="px-0 py-2 flex gap-2">
     <div>
-    <img class="w-[40px] h-[40px] rounded-full object-cover" src=${video.authors[0].profile_picture}>
+    <img class="w-[40px] h-[40px] rounded-full object-cover" src=${
+      video.authors[0].profile_picture
+    }>
   </div>
   <div>
     <p class="text-lg font-bold">${video.title}</p>
-    <div >
+    <div class="flex items-center gap-2">
     <p class="text-gray-600">
       ${video.authors[0].profile_name}
     </p>
-    <img src="" alt="">
+    
+    ${
+      video.authors[0].verified == true
+        ? `<img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=SRJUuaAShjVD&format=png" alt="">`
+        : ""
+    }
   </div>
   </div>
   </div>
